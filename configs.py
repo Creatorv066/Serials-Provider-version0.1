b18d8490527c588a12f471b686e6a1c14151c25e
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-09-27 12:10:27 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -44,7 +44,10 @@ Also remember that developer will Delete Adult Contents from Database. So better
 [Donate Now](https://www.paypal.me/) (PayPal)
   HOME_TEXT = """
 
-    Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
+  Hello, [{}](tg://user?id={})\n\nThis is a Permanent FileStore Bot.
 
-    Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
-"""
+This is Permanent File Store Bot 🗃️.
+
+📢 Send me any File I will give you a permanent Sharable Link. I Support Channel Also! Check About Bot Button.
+
+⚠️ If Do You Want Any Marathi Serial Channel Link MSG Me @iTS_ViSHWA14
