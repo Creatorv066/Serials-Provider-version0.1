b4e7aa4c81cd17610a99d6db2bfc4d413494e831
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-09-27 12:46:35 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -42,8 +42,7 @@ Developer is Super Noob. Just Learning from Official Docs. Please Donate the dev
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
 
-[Donate Now](vishwamore@fam) (UPI)
-"""
+[Donate Now](vishwamore@fam) (UPI)"""
 	HOME_TEXT = """
 Hi, 👋 [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot** 🗃️.
 
