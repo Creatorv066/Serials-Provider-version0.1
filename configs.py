be18cff2d101a565f7b7ca5cd628dd8054d5a8d7
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-09-27 12:02:42 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -45,5 +45,5 @@ Also remember that developer will Delete Adult Contents from Database. So better
   HOME_TEXT = """
    Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
 
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+   Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
 """
