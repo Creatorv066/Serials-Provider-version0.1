bb52057dab372717f337256eb2bf4f0a87a2b8f7
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-09-27 11:05:20 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -45,12 +45,8 @@ Also remember that developer will Delete Adult Contents from Database. So better
 
 [Donate Now](https://www.paypal.me/) (PayPal)
 """
-  HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+	HOME_TEXT = """
+Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
 
-How to Use Bot & it's Benefits??
-
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
 """
