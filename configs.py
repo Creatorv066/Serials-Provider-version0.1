5294d91d1a65fdb526ce0034220423becc247a27
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-09-27 10:15:28 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -1,22 +1,23 @@
 import os
 
-
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "27344784"))
-	API_HASH = os.environ.get("API_HASH", "a12613f54c74d00bfb76cf8f0688c2ee")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6923368496:AAFhmdZNztJWF-0PLwW3dBtl4tvAZUxm_70"))
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "Marathi_Serials_Provider_Bot"))
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002056408106"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6899191648"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Drxfile:Drxfile@drxfile.teugktb.mongodb.net/?retryWrites=true&w=majority"))
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002091923966"))
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002048118352"))
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	ABOUT_BOT_TEXT = f"""
+  API_ID = int(os.environ.get("API_ID", "27344784"))
+  API_HASH = os.environ.get("API_HASH", "a12613f54c74d00bfb76cf8f0688c2ee")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6923368496:AAFhmdZNztJWF-0PLwW3dBtl4tvAZUxm_70")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Marathi_Serials_Provider_Bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "--1002056408106"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "publicearn.com")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "9a696c4ea657a9791396336c3d1d84fafe02eb74")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1562792211"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Drxfile:Drxfile@drxfile.teugktb.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002091923966")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002048118352"))
+  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot! 🗃️
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 **Any Issue Msg Me - @iTS_ViSHWA14** 😎
