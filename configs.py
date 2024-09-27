e124b63c05d5be4cbea5dc27750b10c2c18b6b8e
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-09-27 11:00:28 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -50,6 +50,7 @@ Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
 How to Use Bot & it's Benefits??
 
-📢 Send me any File I will give you a permanent Sharable Link. I Support Channel Also! Check About Bot Button.
+📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
 
-⚠️ If Do You Want Any Marathi Serial Channel Link MSG Me @iTS_ViSHWA14
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+"""
